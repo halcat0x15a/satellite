dbc5c4978d62174082f898a80febb4bba7df9df8
Sanshiro Yoshida <halcat0x15a@gmail.com>
2011-09-22 17:05:04 +0900

    add log

--- satellite.py
+++ satellite.py
@@ -1,30 +1,38 @@
-import time
-from ctypes import *
+import logging
 from ftdi import *
 from PyQt4 import QtGui
 from PyQt4 import QtCore
 
-TIME = 1
-
 BAUDRATE = 38400
+FILENAME = 'satellite.log'
+LENGTH = 1024
 
 class MamiThread(QtCore.QThread):
     def __init__(self, parent, ftdic):
         QtCore.QThread.__init__(self, parent)
         self.ftdic = ftdic
         self.running = True
+        handler = logging.FileHandler(FILENAME)
+        self.logger = logging.getLogger('Mamitterm')
+        self.logger.addHandler(handler)
+        self.logger.setLevel(logging.DEBUG)
     def run(self):
+        self.buffer = ''
         while self.running:
-            buf = ' ' * 1024
-            ftdi_read_data(self.ftdic, buf, len(buf))
-            buf = buf.strip()
-            if buf != "":
-                self.emit(QtCore.SIGNAL('read(QString)'), buf)
+            temp = ' ' * LENGTH
+            ftdi_read_data(self.ftdic, temp, LENGTH)
+            temp = temp.rstrip()
+            self.buffer += temp
+            if temp != '':
+                self.logger.debug(self.buffer)
+                self.emit(QtCore.SIGNAL('read(QString)'), self.buffer)
+                self.buffer = ''
 
 class Mamitterm(QtGui.QWidget):
     def __init__(self, ftdic):
         QtGui.QWidget.__init__(self)
-        self.setWindowTitle('Mikutterm')
+        self.setWindowTitle('Mamitterm')
+        self.connect(self, QtCore.SIGNAL('destroyed()'), self.destroy)
         self.ftdic = ftdic
         vbox = QtGui.QVBoxLayout()
         self.text = QtGui.QTextEdit(self)
@@ -42,9 +50,14 @@ class Mamitterm(QtGui.QWidget):
         self.text.append(buf)
 
     def write(self):
-        text = str(self.line.text())
+        text = str(self.line.text()) + chr(0x0d)
         size = len(text)
         ftdi_write_data(self.ftdic, text, size)
+        self.line.setText('')
+
+    def closeEvent(self, event):
+        self.thread.running = False
+        self.thread.quit()
 
 def main():
     ftdic = ftdi_new()
@@ -57,8 +70,6 @@ def main():
     mamitterm = Mamitterm(ftdic)
     mamitterm.show()
     app.exec_()
-    mamitterm.thread.running = False
-    mamitterm.thread.quit()
     if ftdi_usb_close(ftdic) < 0:
         raise RuntimeError, 'close error'
     ftdi_deinit(ftdic)
